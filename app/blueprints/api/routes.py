bcce050bef88a0fff7fb148907d742be1abd5cb7
KhoaThaiisme <khoathai@khoas-MacBook-Pro.local>
2023-05-16 01:21:38 -0500

    update auth routes and stuff

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -11,10 +11,10 @@ def api_heroes(user):
     heroes = Marvel.query.all()
     for hero in heroes:
         result.append({
-            'id':heroes.id,
-            'name':heroes.name, 
-            'description':heroes.description, 
-            'hero':heroes.user_id
+            'id':hero.id,
+            'name':hero.name, 
+            'description':hero.description, 
+            'hero':hero.user_id
             })
     return jsonify(result), 200
 
@@ -25,24 +25,24 @@ def hero_posts(user, username):
     user = User.query.filter_by(username=username).first()
     if user:
       return jsonify([{
-            'id':heroes.id,
-            'name':heroes.name, 
-            'description':heroes.description, 
-            'hero':heroes.user_id
-              } for heroes in user.marvel_char]), 200
+            'id':hero.id,
+            'name':hero.name, 
+            'description':hero.description, 
+            'hero':hero.user_id
+              } for hero in user.marvel_char]), 200
     return jsonify([{'message':'Invalid Username'}]), 404 
 
 # Send single post
-@bp.get('/hero/<hero_id>')
+@bp.get('/hero/<mar_id>')
 @token_required
 def get_post(user, mar_id):
     try:
-      heroes = Marvel.query.get(mar_id)
+      hero = Marvel.query.get(mar_id)
       return jsonify([{
-            'id':heroes.id,
-            'name':heroes.name, 
-            'description':heroes.description, 
-            'hero':heroes.user_id
+            'id':hero.id,
+            'name':hero.name, 
+            'description':hero.description, 
+            'hero':hero.user_id
               }])
     except: 
       return jsonify([{'message':'Invalid Post Id'}]), 404
@@ -54,12 +54,10 @@ def make_post(user):
     try:
 #  Recieve their post data
         content = request.json
-    #  Create a post instance or entry
-    #  Add foreign key to user id
-        post = Marvel(description=content.get('description'),user_id=user.user_id)
-    # commit our post
-        post.commit()
-    #  return message
-        return jsonify([{'message':'Post Created','body':post.body}])
+        marv = Marvel(description=content.get('description'),user_id=user.user_id)
+
+        marv.commit()
+  
+        return jsonify([{'message':'Post Created','description':marv.description}])
     except:
        jsonify([{'message':'invalid form data'}]), 401
